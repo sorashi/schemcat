9f2f1e593395601800f3ccb7074e4bacfab82f1b
Dennis Prazak <prazak.dennis@gmail.com>
2022-10-02 15:49:56 +0200

    Extract function to file-scope

--- schemcat/src/components/SvgDiamondShape.tsx
+++ schemcat/src/components/SvgDiamondShape.tsx
@@ -1,8 +1,8 @@
+function getPolygonPoints(props: any) {
+    const { x = 0, y = 0, width = 0, height = 0 } = props
+    return `${x},${y + height / 2} ${x + width / 2},${y} ${x + width},${y + height / 2} ${x + width / 2},${y + height}`
+}
 function SvgDiamondShape(props: any) {
-    function getPolygonPoints(props: any) {
-        const { x = 0, y = 0, width = 0, height = 0 } = props
-        return `${x},${y + height / 2} ${x + width / 2},${y} ${x + width},${y + height / 2} ${x + width / 2},${y + height}`
-    }
     return (
         <polygon {...props} points={getPolygonPoints(props)}></polygon>
     )
