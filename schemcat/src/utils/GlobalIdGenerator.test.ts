cc1836260943d96036ce4112515337338a82c11c
Dennis Prazak <prazak.dennis@gmail.com>
2022-10-12 12:03:48 +0200

    Configure testing, add example tests

---
+++ schemcat/src/utils/GlobalIdGenerator.test.ts
@@ -0,0 +1,14 @@
+import globalIdGenerator from "./GlobalIdGenerator"
+
+describe("GlobalIdGenerator", () => {
+    test("First ID is 1", () => {
+        expect(globalIdGenerator.nextId()).toBe(1)
+    })
+    test("Next ID is 2", () => {
+        expect(globalIdGenerator.nextId()).toBe(2)
+    })
+    test("Reset ID resets to 1", () => {
+        globalIdGenerator.resetId()
+        expect(globalIdGenerator.nextId()).toBe(1)
+    })
+})
