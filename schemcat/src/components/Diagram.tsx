f54b1d6bc8114ff43f97e9e13a29269b8e8cd0c0
Dennis Prazak <prazak.dennis@gmail.com>
2022-10-30 19:52:06 +0100

    Remove unused import

--- schemcat/src/components/Diagram.tsx
+++ schemcat/src/components/Diagram.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef, useState } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Connection, ErNode as ErNodeModel, ErNodeType } from "../model/DiagramModel"
 import ErNode from "./ErNode"
 import SvgConnection from "./SvgConnection"
