49a6d3beba4dcc7bf7ab24351049b1a5f5177a14
Dennis Prazak <prazak.dennis@gmail.com>
2023-03-01 11:12:31 +0100

    Add draft of Schemcat model

---
+++ schemcat/src/model/SchemcatModel.ts
@@ -0,0 +1,57 @@
+import { Cardinality, DiagramModel } from './DiagramModel'
+
+export class SchemaCategory {
+  objects: SchemaObject[] = []
+  morphisms: Morphism[] = []
+}
+
+export class SchemaObject {
+  key = -1
+  label = ''
+  data: Set<number> = new Set()
+  identifiers: Set<Set<number>> = new Set()
+}
+
+export class Morphism {
+  signature: number[] = []
+  domain = -1
+  codomain = -1
+  direction = 1
+  label = ''
+  cardinality: Cardinality = new Cardinality()
+  duplicities = false
+  ordering = false
+}
+
+function erDiagramToSchemcat(diagram: DiagramModel): SchemaCategory {
+  const schema = new SchemaCategory()
+
+  // objects and identifiers
+  diagram.nodes.forEach((node) => {
+    const schemaObject = new SchemaObject()
+    schemaObject.key = node.id
+    schemaObject.label = node.label
+    schemaObject.identifiers = new Set(
+      diagram.identifiers.filter((id) => id.identifies === node.id).map((id) => id.identities)
+    )
+    // todo: add data
+    schemaObject.data = new Set()
+    schema.objects.push(schemaObject)
+  })
+
+  // morphisms
+  diagram.links.forEach((link) => {
+    const morphism = new Morphism()
+    morphism.domain = link.fromId
+    morphism.codomain = link.toId
+    // signature
+    morphism.signature = []
+    morphism.label = ''
+    morphism.cardinality = link.multiplicity
+    morphism.duplicities = false
+    morphism.ordering = false
+    schema.morphisms.push(morphism)
+  })
+
+  return schema
+}
