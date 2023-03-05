5392a480a4d139b528b0a23f7a682101f84d5bcc
Dennis Prazak <prazak.dennis@gmail.com>
2023-03-05 17:17:50 +0100

    Rename multiplicity to cardinality

--- schemcat/src/components/CardinalityText.tsx
+++ schemcat/src/components/CardinalityText.tsx
@@ -2,9 +2,9 @@ import React, { useLayoutEffect, useRef, useState } from 'react'
 import { Cardinality } from '../model/DiagramModel'
 import { plainToInstance } from 'class-transformer'
 
-export function CardinalityText(props: { multiplicity: Cardinality; x: number; y: number; pathId: string }) {
+export function CardinalityText(props: { cardinality: Cardinality; x: number; y: number; pathId: string }) {
   const { x, y } = props
-  const multiplicity = plainToInstance(Cardinality, props.multiplicity)
+  const cardinality = plainToInstance(Cardinality, props.cardinality)
   const [textWidth, setTextWidth] = useState(0)
   const [textHeight, setTextHeight] = useState(0)
   const textRef = useRef<SVGTextElement>(null)
@@ -15,11 +15,11 @@ export function CardinalityText(props: { multiplicity: Cardinality; x: number; y
       setTextHeight(height)
     }
   }, [textRef])
-  if (multiplicity.isDefault()) return null
+  if (cardinality.isDefault()) return null
   return (
     <text ref={textRef} x={0} y={0} dy={-5} dominantBaseline='auto' textAnchor='middle'>
       <textPath href={`#${props.pathId}`} startOffset='35'>
-        {multiplicity.lowerBound}..{multiplicity.upperBound}
+        {cardinality.lowerBound}..{cardinality.upperBound}
       </textPath>
     </text>
   )
