975030d854e2af3ed6ff0027d7145a46dae1d422
Dennis Prazak <prazak.dennis@gmail.com>
2023-03-23 10:41:16 +0100

    Make sure project name is non-empty

--- schemcat/src/components/EditableText.tsx
+++ schemcat/src/components/EditableText.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useLayoutEffect, useRef, useState } from 'react'
+import { isOnlyWhitespace } from '../utils/String'
 
 interface EditableTextProps {
   onChange?: React.ChangeEventHandler<HTMLInputElement> // for now
@@ -10,10 +11,12 @@ export function EditableText({ onChange, initialText }: EditableTextProps) {
   const [size, setSize] = useState<number | undefined>(undefined)
   useLayoutEffect(() => {
     if (!inputRef.current) return
-    inputRef.current.size = inputRef.current.value.length
+    if (inputRef.current) setSize(Math.max(10, inputRef.current.value.length))
   }, [inputRef.current?.value])
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    if (inputRef.current) setSize(Math.max(1, inputRef.current.value.length))
+    if (inputRef.current) setSize(Math.max(10, inputRef.current.value.length))
+    // ensure text is non-empty
+    if (isOnlyWhitespace(e.target.value)) return
     if (onChange) onChange(e)
   }
   return (
@@ -22,6 +25,7 @@ export function EditableText({ onChange, initialText }: EditableTextProps) {
       type='text'
       onChange={handleChange}
       defaultValue={initialText}
+      placeholder={'Project Name'}
       size={size}
       className='focus:text-black text-gray-600 w-auto inline-block text-center'
     />
