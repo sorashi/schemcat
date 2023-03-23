1c9f7351470e1219748dd18f500a11dcf52b97f4
Dennis Prazak <prazak.dennis@gmail.com>
2023-03-23 13:34:37 +0100

    Make schemcat connections undirected

--- schemcat/src/components/SchemcatDiagram.tsx
+++ schemcat/src/components/SchemcatDiagram.tsx
@@ -40,18 +40,6 @@ function SchemcatDiagram({ isSelectedNodeInActiveTabSet }: SchemcatDiagramProps)
           <EmptyTriangleMarker />
           <TwoSidedMarker />
         </defs>
-        {schemcat.objects.map((o) => {
-          const node = nodes.find((x) => x.id === o.key)
-          if (!node) return null
-          return (
-            <React.Fragment key={`raw-schemcat-object-${o.key}`}>
-              <circle cx={node.x} cy={node.y} r={10} stroke='black' strokeWidth={1} fill='white'></circle>
-              <text x={node.x + 12} y={node.y + 5}>
-                {o.label}
-              </text>
-            </React.Fragment>
-          )
-        })}
         {schemcat.morphisms.map((m) => {
           const from = nodes.find((n) => n.id === m.domain)
           const to = nodes.find((n) => n.id === m.codomain)
@@ -61,20 +49,25 @@ function SchemcatDiagram({ isSelectedNodeInActiveTabSet }: SchemcatDiagramProps)
           const cardinalityPosition = fromPos.add(toPos.subtract(fromPos).multiply(0.5))
           return (
             <React.Fragment key={`raw-schemcat-morphism-${m.domain}-${m.codomain}-${m.direction}`}>
-              <line
-                x1={from.x}
-                y1={from.y}
-                x2={to.x}
-                y2={to.y}
-                markerEnd={`url(#${twoSidedMarkerId})`}
-                stroke='rgba(50,50,50,0.5)'
-                strokeWidth={1}></line>
+              <line x1={from.x} y1={from.y} x2={to.x} y2={to.y} stroke='rgba(50,50,50,0.5)' strokeWidth={1}></line>
               <CardinalityText
                 x={cardinalityPosition.x}
                 y={cardinalityPosition.y}
                 cardinality={m.cardinality}
                 pathId={''}
               />
+            </React.Fragment>
+          )
+        })}
+        {schemcat.objects.map((o) => {
+          const node = nodes.find((x) => x.id === o.key)
+          if (!node) return null
+          return (
+            <React.Fragment key={`raw-schemcat-object-${o.key}`}>
+              <circle cx={node.x} cy={node.y} r={10} stroke='black' strokeWidth={1} fill='white'></circle>
+              <text x={node.x + 12} y={node.y + 5}>
+                {o.label}
+              </text>
             </React.Fragment>
           )
         })}
