a23ae1347faace91381598acc48f67a64f8f19f1
Dennis Prazak <prazak.dennis@gmail.com>
2023-03-28 00:19:56 +0200

    Add regression test for #44

--- schemcat/cypress/e2e/spec.cy.ts
+++ schemcat/cypress/e2e/spec.cy.ts
@@ -6,4 +6,16 @@ describe('regression', () => {
     cy.get('dt').contains('label').next().find('input').focus().type('{selectAll}{backspace}Human').blur()
     cy.get('svg span').should('contain.text', 'Human')
   })
+  it('#44: ExportSvgDialog dialog interaction available', () => {
+    cy.visit('/')
+
+    cy.get('ul').contains('File').click()
+    cy.get('ul').contains('Export as').click()
+    cy.get('ul').contains('SVG').click()
+    cy.get('form').contains('Include serialized diagram').click()
+
+    cy.get('form').contains('Include serialized diagram').find('input').its('0.checked').should('be.false')
+
+    cy.get('form').get('button').contains('Cancel').click()
+  })
 })
