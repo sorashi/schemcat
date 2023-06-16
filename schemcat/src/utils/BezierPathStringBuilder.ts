757697a98f4368a4b9ba43ae6f67c2e36d1f24b5
Dennis Prazak <prazak.dennis@gmail.com>
2023-06-16 11:32:54 +0200

    Create more genenral SVG path builder

--- schemcat/src/utils/BezierPathStringBuilder.ts
+++ schemcat/src/utils/BezierPathStringBuilder.ts
@@ -1,28 +1,29 @@
+import SvgPathStringBuilder from './SvgPathStringBuilder'
 import Vector2 from './Vector2'
 
 export default class BezierPathStringBuilder {
-  private path = ''
   private bezierStarted = false
+  private pathBuilder: SvgPathStringBuilder = new SvgPathStringBuilder()
 
   addStart(from: Vector2) {
     if (this.bezierStarted) throw new Error('bezier start was already specified')
-    this.path += `M ${from}`
+    this.pathBuilder.start(from)
     this.bezierStarted = true
   }
 
   addFirstBezier(from: Vector2, controlPoint1: Vector2, controlPoint2: Vector2, to: Vector2) {
     this.addStart(from)
-    this.addBezier(controlPoint1, controlPoint2, to)
+    this.pathBuilder.cubicBezier(controlPoint1, controlPoint2, to)
   }
 
   addReflectedBezier(controlPoint2: Vector2, to: Vector2) {
     this.ensureStarted()
-    this.path += ` S ${controlPoint2}, ${to}`
+    this.pathBuilder.cubicBezierSymmetric(controlPoint2, to)
   }
 
   addBezier(controlPoint1: Vector2, controlPoint2: Vector2, to: Vector2) {
     this.ensureStarted()
-    this.path += ` C ${controlPoint1}, ${controlPoint2}, ${to}`
+    this.pathBuilder.cubicBezier(controlPoint1, controlPoint2, to)
   }
 
   private ensureStarted() {
@@ -30,6 +31,6 @@ export default class BezierPathStringBuilder {
   }
 
   toString() {
-    return this.path
+    return this.pathBuilder.toString()
   }
 }
---
+++ schemcat/src/utils/SvgPathStringBuilder.ts
@@ -0,0 +1,108 @@
+import Vector2 from './Vector2'
+
+export default class SvgPathStringBuilder {
+  private path: string[] = []
+  start(location: Vector2) {
+    this.move(location)
+  }
+  move(location: Vector2) {
+    this.path.push(`M ${location}`)
+  }
+  moveRelative(by: Vector2) {
+    this.path.push(`m ${by}`)
+  }
+  lineTo(location: Vector2) {
+    this.path.push(`L ${location}`)
+  }
+  lineToRelative(by: Vector2) {
+    this.path.push(`l ${by}`)
+  }
+  horizontalLine(x: number) {
+    this.path.push(`H ${x}`)
+  }
+  horizontalLineRelative(byX: number) {
+    this.path.push(`h ${byX}`)
+  }
+  verticalLine(y: number) {
+    this.path.push(`V ${y}`)
+  }
+  verticalLineRelative(byY: number) {
+    this.path.push(`v ${byY}`)
+  }
+  /** "Closes" the path by drawing a straight line from the current position to the starting point.
+   * Usually used at the end of a path, but can be used anywhere.
+   */
+  close() {
+    this.path.push('Z')
+  }
+
+  cubicBezier(controlPoint1: Vector2, controlPoint2: Vector2, end: Vector2) {
+    this.path.push(`C ${controlPoint1}, ${controlPoint2}, ${end}`)
+  }
+  cubicBezierRelative(controlPoint1: Vector2, controlPoint2: Vector2, end: Vector2) {
+    this.path.push(`c ${controlPoint1}, ${controlPoint2}, ${end}`)
+  }
+  /** Infers the first control point by reflecting the last control point of the previous curve.
+   * If there is no previous curve, uses the last cursor position as the first control point.
+   */
+  cubicBezierSymmetric(controlPoint2: Vector2, end: Vector2) {
+    this.path.push(`S ${controlPoint2}, ${end}`)
+  }
+  /** Infers the first control point by reflecting the last control point of the previous curve.
+   * If there is no previous curve, uses the last cursor position as the first control point.
+   */
+  cubicBezierSymmetricRelative(controlPoint2: Vector2, end: Vector2) {
+    this.path.push(`s ${controlPoint2}, ${end}`)
+  }
+  quadraticBezier(controlPoint: Vector2, end: Vector2) {
+    this.path.push(`Q ${controlPoint}, ${end}`)
+  }
+  quadraticBezierRelative(controlPoint: Vector2, end: Vector2) {
+    this.path.push(`q ${controlPoint}, ${end}`)
+  }
+  /** Infers the control point from the previous control point.
+   * Previous command must have been a quadratic bezier or another symmetric quadratic bezier.
+   */
+  quadraticBezierSymmetric(end: Vector2) {
+    this.path.push(`T ${end}`)
+  }
+  /** Infers the control point from the previous control point.
+   * Previous command must have been a quadratic bezier or another symmetric quadratic bezier.
+   */
+  quadraticBezierSymmetricRelative(end: Vector2) {
+    this.path.push(`t ${end}`)
+  }
+
+  /**
+   * @param radiusX the x radius of the ellipse from which to draw the arc
+   * @param radiusY the y radius of the ellipse from which to draw the arc
+   * @param xAxisRotation rotation (in degrees) of the x axis of the ellipse from which to draw the arc
+   * @param largeArc true if the arc should be greater than 180 degrees
+   * @param sweep true if the arc should begin moving at positive angles, otherwise negative ones
+   * @param location location of the arc
+   */
+  arc(radiusX: number, radiusY: number, xAxisRotation: number, largeArc: boolean, sweep: boolean, location: Vector2) {
+    this.path.push(`A ${radiusX} ${radiusY} ${xAxisRotation} ${largeArc ? 1 : 0} ${sweep ? 1 : 0} ${location}`)
+  }
+  arcRelative(
+    radiusX: number,
+    radiusY: number,
+    xAxisRotation: number,
+    largeArc: boolean,
+    sweep: boolean,
+    location: Vector2
+  ) {
+    this.path.push(`a ${radiusX} ${radiusY} ${xAxisRotation} ${largeArc ? 1 : 0} ${sweep ? 1 : 0} ${location}`)
+  }
+
+  // returning
+  getPath(): string {
+    return this.path.join(' ')
+  }
+  finish(): string {
+    return this.path.join(' ')
+  }
+  toString(): string {
+    return this.path.join(' ')
+  }
+}
